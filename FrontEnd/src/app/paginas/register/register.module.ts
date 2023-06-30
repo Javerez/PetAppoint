3d699ad0e9cc4d74970abe3c8ba8323a241d817d
Javerez <javierperez0108@gmail.com>
2023-06-29 23:42:05 -0400

    formulario registro

--- FrontEnd/src/app/paginas/register/register.module.ts
+++ FrontEnd/src/app/paginas/register/register.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 
 import { RegisterPageRoutingModule } from './register-routing.module';
@@ -11,10 +11,11 @@ import { RegisterPage } from './register.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     RegisterPageRoutingModule,
-    ReactiveFormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [RegisterPage]
 })
--- FrontEnd/src/app/paginas/register/register.page.ts
+++ FrontEnd/src/app/paginas/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Servicios } from 'src/app/servicios/services';
+
 
 @Component({
   selector: 'app-register',
@@ -9,9 +11,18 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class RegisterPage implements OnInit {
   formRegister!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, services: Servicios) { 
+    this.formRegister = this.formBuilder.group({
+      'nombre': new FormControl("", Validators.required),
+      'apellido': new FormControl("", Validators.required),
+      'email': new FormControl("", Validators.required),
+      'clave': new FormControl("", Validators.required)
+    });
+  }
 
   ngOnInit() {
   }
-
+  mostrar(){
+    console.log(this.formRegister.value)
+  }
 }
